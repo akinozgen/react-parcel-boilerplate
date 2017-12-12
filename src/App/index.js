a34f3c2a38e55ac47d2a52ede91be1752be43d1c
Akın Özgen <akinozgen17@outlook.com>
2017-12-12 05:49:00 +0300

    intro page

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import logo from '../../images/package.svg';
 
 export default class App extends React.Component {
     render() {
-        return <h1>Deneme</h1>;
+        return (
+            <div className="container">
+                <img src={logo}/>
+            </div>
+        );
     }
 }
